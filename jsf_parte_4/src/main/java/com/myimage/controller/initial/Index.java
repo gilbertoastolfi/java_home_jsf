befeaf0d87a216e57bc562118879a347eb798994
trabalho <trabalho@gilberto-PC>
2015-11-16 09:32:09 -0300

    Criando filtro para conexão com BD

---
+++ jsf_parte_4/src/main/java/com/myimage/controller/initial/Index.java
@@ -0,0 +1,19 @@
+package com.myimage.controller.initial;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+ 
+@RequestScoped
+@ManagedBean
+public class Index {
+ 
+   @PostConstruct
+   public void init(){
+      System.out.println("Bean executado!");
+   }
+ 
+   public String getMessage(){
+      return "Hello World JSF!";
+   }
+}
